660ccb018dcc08ce37f26b1277be49770e11149d
James Scott <jsa.codes@gmail.com>
2022-06-09 16:31:11 -0500

    Still trying to figure out why HTML is not rendering. Have doubled checked all JS files and everything appears to be ok. Thinking it is index.html at this point.

--- scripts/FishList.js
+++ scripts/FishList.js
@@ -8,19 +8,23 @@ export const FishList = () => {
   const fishes = getFish();
 
   // Create HTML representation of each fish
-  const htmlString = '<article class= "fishList">';
+  const htmlString = `<article class= "fishList">`;
   // Iterate the array of fish objects - building the html from the key value pairs in the array of fish objects
   for (const fish of fishes) {
-    htmlString += `<section class="fish__card">
-            <div><img class="fish__image image--card" src="${fish.image}" /></div>
-            <div class="fish__name">${fish.name}</div>
-            <div class="fish__species">${fish.species}</div>
-            <div class="fish__length">${fish.length}</div>
-            <div class="fish__location">${fish.location}</div>
-            <div class="fish__diet">${fish.food}</div>
+    htmlString += `<section class="fishCard">
+          <div>
+            <img class="fishImages image__card" src="${fish.image}" />
+          </div>
+          <div class="fishInfo">
+            <div class="fish__name fishInfo">Name: ${fish.name}</div>
+            <div class="fish__species fishInfo">Species: ${fish.species}</div>
+            <div class="fish__length fishInfo">Length: ${fish.length}</div>
+            <div class="fish__location fishInfo">Location: ${fish.location}</div>
+            <div class="fish__diet fishInfo">Diet: ${fish.diet}</div>
+          </div>
         </section>
-`;
+        `;
   }
-  htmlString += '</article>';
+  htmlString += `</article>`;
   return htmlString;
 };
